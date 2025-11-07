6d683cc10e2d0d06a378851cc1b320c0b3f42bec
Dominic Garms <djgarms@gmail.com>
2025-11-07 09:10:36 +0800

    add file

---
+++ src/hooks/useNhostAuthElevated.ts
@@ -0,0 +1,84 @@
+import type { NhostClient } from "@nhost/nhost-js";
+import type { ErrorResponse } from "@nhost/nhost-js/auth";
+import { FetchError } from "@nhost/nhost-js/fetch";
+import { useCallback, useState } from "react";
+import { useNhost } from "./useNhost";
+import { useNhostSecurity } from "./useNhostSecurity";
+
+type AuthMethods = {
+  [K in keyof NhostClient["auth"]]: NhostClient["auth"][K] extends (
+          ...args: any[]
+      ) => any
+      ? NhostClient["auth"][K]
+      : never;
+};
+
+type AuthFnName = keyof AuthMethods;
+
+type ParamsOf<K extends AuthFnName> = Parameters<AuthMethods[K]>[0];
+type DataOf<K extends AuthFnName> = Awaited<ReturnType<AuthMethods[K]>>;
+
+interface UseAuthHandlerOptions<K extends AuthFnName> {
+  fn: K;
+  onSuccess?: ({
+                 nhost,
+                 data,
+                 params
+               }: {
+    nhost: NhostClient;
+    data: DataOf<K>;
+    params: ParamsOf<K>;
+  }) => Promise<void | undefined> | void;
+  onError?: ({
+               nhost,
+               error,
+               params
+             }: {
+    nhost: NhostClient;
+    error: FetchError<ErrorResponse>;
+    params: ParamsOf<K>;
+  }) => Promise<void | undefined> | void;
+}
+
+export function useNhostAuthElevated<K extends AuthFnName>({
+                                                     fn,
+                                                     onSuccess,
+                                                     onError,
+                                                   }: UseAuthHandlerOptions<K>) {
+  const {nhost} = useNhost();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [error, setError] = useState<FetchError<ErrorResponse> | null>(null);
+  const { requiresElevation, checkElevation } = useNhostSecurity();
+
+  const callAsync = useCallback(
+      async (
+          params: ParamsOf<K>
+      ): Promise<DataOf<K> | FetchError<ErrorResponse>> => {
+        setIsLoading(true);
+        setError(null);
+        setIsSuccess(false);
+
+        try {
+          if(requiresElevation) {
+            await checkElevation();
+          }
+          const fnToCall = nhost.auth[fn] as AuthMethods[K];
+          const result = await fnToCall(params);
+          if (onSuccess) await onSuccess({nhost, data: result, params});
+          setIsSuccess(true);
+          return result;
+        } catch (e) {
+          const fetchError = e as FetchError<ErrorResponse>;
+          setError(fetchError);
+          if (onError) await onError({nhost, error: fetchError, params});
+          return fetchError;
+        } finally {
+          setIsLoading(false);
+        }
+      },
+      [nhost, fn, onSuccess, onError]
+  );
+
+  return {callAsync, isLoading, isSuccess, error};
+}
